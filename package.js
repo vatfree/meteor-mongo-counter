da774752d476fb9f766c944783367d6e3462c9ce
Niklas Dahlheimer <niklasdahlheimer@web.de>
2022-03-26 21:10:50 +0100

    update version code

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
     name: 'niklasdada:mongo-counter',
     summary: 'Atomic counters stored in MongoDB (rewritten in JS, added getCounter(), compatible with Meteor 2.6)',
-    version: '1.0.0',
-    git: 'https://github.com/niklasdahlheimer/meteor-mongo-counter',
+    version: '1.0.1',
+    git: 'https://github.com/niklasdahlheimer/meteor-mongo-counter.git',
 })
 
 Package.onUse(function (api) {
