5ebb91bb16ac2dd61a12ac2b4d8665eecba4fa52
Niklas Dahlheimer <niklasdahlheimer@web.de>
2022-03-26 20:53:38 +0100

    reformat code

--- package.js
+++ package.js
@@ -1,27 +1,27 @@
 Package.describe({
-  name: 'niklasdahlheimer:mongo-counter',
-  summary: 'Atomic counters stored in MongoDB',
-  version: '1.0.0',
-  git: 'https://github.com/niklasdahlheimer/meteor-mongo-counter',
+    name: 'niklasdahlheimer:mongo-counter',
+    summary: 'Atomic counters stored in MongoDB (rewritten in JS)',
+    version: '1.0.0',
+    git: 'https://github.com/niklasdahlheimer/meteor-mongo-counter',
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@2.0')
-  api.use(['ecmascript'], 'server')
-  api.addFiles('counter.js', 'server')
-  if (api.export) {
-    api.export('incrementCounter', 'server')
-    api.export('decrementCounter', 'server')
-    api.export('setCounter', 'server')
-    api.export('getCounter', 'server')
-    api.export('deleteCounters', 'server', { testOnly: true })
-  }
+    api.versionsFrom('METEOR@2.0')
+    api.use(['ecmascript'], 'server')
+    api.addFiles('counter.js', 'server')
+    if (api.export) {
+        api.export('incrementCounter', 'server')
+        api.export('decrementCounter', 'server')
+        api.export('setCounter', 'server')
+        api.export('getCounter', 'server')
+        api.export('deleteCounters', 'server', {testOnly: true})
+    }
 })
 
 Package.onTest(function (api) {
-  api.use('ecmascript')
-  api.use('tinytest')
-  api.use('mongo')
-  api.use('niklasdahlheimer:mongo-counter')
-  api.addFiles('counter.tests.js', 'server');
+    api.use('niklasdahlheimer:mongo-counter')
+    api.use('ecmascript')
+    api.use('mongo')
+    api.use('tinytest')
+    api.addFiles('counter.tests.js', 'server');
 })
